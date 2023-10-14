51dab389ec49824846235c36dbccf12192dd52a0
JDBhogate <jaydeepbhogate@gmail.com>
2023-10-14 19:43:46 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,46 @@
+import Dosa from '../images/dosa.jpg'
+import Chola from '../images/chhola.jpg'
+import Idli from '../images/idli.jpg'
+import MasalaDosa from '../images/masala.jpg'
+import Paneer from '../images/paneer.jpg'
+import Gujrati from '../images/gujrati.jpeg'
+
+export const MenuList = [
+{
+    name:'Dosa',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    image:Dosa,
+    price:200
+},
+{
+    name:'Chola',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    image:Chola,
+    price:250
+},
+{
+    name:'Idli',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    image:Idli,
+    price:300
+},
+{
+    name:'MasalaDosa',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    image:MasalaDosa,
+    price:350
+},
+{
+    name:'Paneer',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    image:Paneer,
+    price:400
+},
+{
+    name:'Gujrati',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod',
+    image:Gujrati,
+    price:450
+},
+
+];
